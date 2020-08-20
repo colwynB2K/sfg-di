29b8df3e942a3936ba139ef3c97a2df8c66af826
Ivo Eersels <ivo.eersels@gmail.com>
2020-08-20 11:57:52 +0200

    Dependency Injection WITH Spring - Primary Beans

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -14,10 +14,9 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
+		System.out.println("----- Primary Injection");
 		MyController myController = (MyController) ctx.getBean("myController"); // Never explicitly created this object via new MyController();, due to IoC Spring is handling the construction of this object
-
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println(myController.sayHello());
 
 		System.out.println("----- Property Injection");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
--- src/main/java/guru/springframework/sfgdi/controller/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -1,13 +1,18 @@
 package guru.springframework.sfgdi.controller;
 
+import guru.springframework.sfgdi.service.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Hello World!!!");
+    private final GreetingService greetingService;
 
-        return "Hi folks!";
+    public MyController(GreetingService greetingService) { // Using Constructor injection without specifying @Autowired, we also don't use a qualifier to specify which GreetingService implementation we want
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
