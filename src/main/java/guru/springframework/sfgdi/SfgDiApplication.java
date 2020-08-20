a86757abcd538b47eed07e3a2f92409d1a89b124
Ivo Eersels <ivo.eersels@gmail.com>
2020-08-20 09:25:51 +0200

    Simple Hello World example (of Inversion of Control)

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController"); // Never explicitly created this object via new MyController();, due to IoC Spring is handling the construction of this object
+
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
