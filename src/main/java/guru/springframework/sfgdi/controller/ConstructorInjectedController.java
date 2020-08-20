a8b96f53b21348f3bb3c9211d2a3b70f5fbf7653
Ivo Eersels <ivo.eersels@gmail.com>
2020-08-20 11:17:45 +0200

    Dependency Injection example WITH Spring

--- src/main/java/guru/springframework/sfgdi/controller/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controller/ConstructorInjectedController.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgdi.controller;
 
 import guru.springframework.sfgdi.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService; // NOTE the final keyword here which prevents that this variable is changed after it was assigned
 
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
